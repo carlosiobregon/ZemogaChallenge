e4ec7c5265876e24d86a41ab4d6154056ac1210e
Carlos Obregon <carlos.obregon@vusecurity.com>
2022-10-09 16:49:03 -0500

    Create view for when there are no posts

---
+++ ZemogaChallenge/NoPostView.swift
@@ -0,0 +1,104 @@
+//
+//  NoPostView.swift
+//  ZemogaChallenge
+//
+//  Created by Carlos Obregon on 9/10/22.
+//
+
+import UIKit
+
+protocol INoPostView {
+    func retryButtonTouched()
+}
+
+class NoPostView: UIView {
+    
+    //MARK: - Attributes
+    private let lateralMargin: CGFloat = 35
+    private let verticalMargin: CGFloat = 20
+    private let buttonHeight: CGFloat = 35
+    private let buttonWidth: CGFloat = 32
+    
+    
+    //MARK: - Outlets
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+      }()
+    
+    private lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = .zero
+        return label
+    }()
+    
+    private lazy var customButton: UIButton = {
+        let button = UIButton()
+        button.layer.shadowOffset = CGSize(width: .zero, height: 8)
+        button.layer.shadowRadius = 16
+        button.layer.shadowOpacity = 1
+        button.layer.cornerRadius = 8
+        button.layer.masksToBounds = false
+        
+        button.addTarget(self, action: #selector(retryButtonTouched), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    //MARK: - Delegate
+    var delegate: INoPostView?
+    
+    //MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        applyViewCode()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        applyViewCode()
+    }
+    
+    //Mark: - Actions
+    @objc private func retryButtonTouched() {
+        delegate?.retryButtonTouched()
+    }
+    
+}
+
+
+//Mark: - ViewCodeConfiguration
+extension NoPostView: ViewCodeConfiguration {
+    func buildHierarchy() {
+        addSubviews(titleLabel, subtitleLabel, customButton)
+    }
+    
+    func setupConstraints() {
+        
+        NSLayoutConstraint.activate([
+            subtitleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            subtitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: lateralMargin),
+            subtitleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -lateralMargin)
+        ])
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: subtitleLabel.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: subtitleLabel.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: subtitleLabel.topAnchor, constant: -verticalMargin)
+        ])
+        
+        NSLayoutConstraint.activate([
+            customButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            customButton.heightAnchor.constraint(equalToConstant: buttonHeight),
+            customButton.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: verticalMargin)
+        ])
+        
+    }
+    
+}
+
+
